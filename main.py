6c7bb76d41cc33e4056ce5ee3600f37ebf574260
Florez16 <58142370+Florez16@users.noreply.github.com>
2023-11-25 20:39:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 from datetime import datetime
 import streamlit as st
+import pytz
 
 
 def get_ma_prices(period1, period2):
@@ -14,12 +15,12 @@ def get_ma_prices(period1, period2):
     data['MA_5'] = data['Close'].rolling(window=5).mean()
     data['MA_20'] = data['Close'].rolling(window=20).mean()
 
-    #print(data.head())
+    # print(data.head())
 
     last_row = data.iloc[-1]
 
     # Extract the desired values
-    #close_price = last_row['Close']
+    # close_price = last_row['Close']
     ma_5 = last_row['MA_5']
     ma_20 = last_row['MA_20']
 
@@ -33,25 +34,6 @@ def get_signal(ma1_price, ma2_price):
 
     return False
 
-def gen_message(ma1, ma2, ma1_price, ma2_price, long_signal, current_datetime):
-    """
-    Generate message for display
-    """
-    msg = \
-        f"""
-            {current_datetime}
-            QQQ Latest MA{ma1}: ${round(ma1_price, 2)}
-            QQQ Latest MA{ma2}: ${round(ma2_price, 2)}
-            Long Signal: {long_signal}
-        """
-
-    if long_signal:
-        msg += "\tCan buy TQQQ if you haven't do so."
-    else:
-        msg += "\tTime to sell TQQQ."
-
-    return msg
-
 
 def main():
     ma1 = 5
@@ -59,21 +41,34 @@ def main():
 
     # Getting the close, ma5 and ma20 prices of the latest trading day
     ma1_price, ma2_price = get_ma_prices(ma1, ma2)
-    #print(f"MA5: {ma1_price}, MA20: {ma2_price}")
+    # print(f"MA5: {ma1_price}, MA20: {ma2_price}")
 
     long_signal = get_signal(ma1_price, ma2_price)
-    #print(long_signal)
+    # print(long_signal)
 
     # Get the current date and time
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M")
+    gmt8 = pytz.timezone('Asia/Singapore')
+    current_datetime = datetime.now(gmt8).strftime("%Y-%m-%d %H:%M")
 
     # Print the current date and time
-    #print("Current Date and Time:", current_datetime)
+    # print("Current Date and Time:", current_datetime)
 
-    # Get message
-    message = gen_message(ma1, ma2, ma1_price, ma2_price, long_signal, current_datetime)
-    st.write(message)
-    print(message)
+    # Display message
+    st.title(current_datetime)
+    st.markdown(f"QQQ Latest MA{ma1}: ${round(ma1_price, 2)}")
+    st.markdown(f"QQQ Latest MA{ma2}: ${round(ma2_price, 2)}")
 
-st.write("TEST")
+    # Determine color for displaying buy/sell actions
+    color = "red"
+    if long_signal:
+        color = "green"
+
+    st.markdown(f'<p style="color: {color};">Long Signal: {long_signal}</p>', unsafe_allow_html=True)
+
+    if long_signal:
+        st.markdown(f'<b><p style="color: {color};">Can buy TQQQ if not yet in cycle.</p><b>', unsafe_allow_html=True)
+    else:
+        st.markdown(f'<b><p style="color: {color};">Time to sell TQQQ.</p><b>', unsafe_allow_html=True)
+
+
 main()
